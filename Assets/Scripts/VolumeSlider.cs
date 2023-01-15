9e00b855dde17063ba4391c01878bc108ecdad2d
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-01-14 20:45:13 -0500

    feat: :sparkles: Music slider changes volume
    the music is ALIIVVVEE!!!!!!!!!

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -1,27 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
 {
     // Start is called before the first frame update
     // Create a integer variable that is public called default volume
-    public static int defaultVolume = 45;
+    public static float defaultVolume = 0.45f;
+    private float currentVolume = 0.10f;
     public GameObject Music;
+    public Slider slider;
+
+    private AudioSource MusicAudioComponent;
     void Start()
     {
         // On start we want set the volume to the default
         // Find the BackgroundMusic Game Object.
-        Music = GameObject.Find("Music");
-        if (Music != null) {
+        Music = GameObject.Find("/BackgroundMusic");
+        if (Music == null) {
             print(Music);
-            return null;
+            return;
         }
-       Component MusicAudioComponent = Music.GetComponent<AudioSource>();
-        if (MusicAudioComponent != null) {
+       MusicAudioComponent = Music.GetComponent<AudioSource>();
+        if (MusicAudioComponent == null) {
             print(MusicAudioComponent);
-            return null;
+            return;
         }
+        MusicAudioComponent.volume = defaultVolume;
     }
 
     // Update is called once per frame
@@ -29,7 +35,9 @@ public class VolumeSlider : MonoBehaviour
     {
         /* check to see the position of the slider 
         and set the volume at that position. */
-        
 
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+        currentVolume = slider.value;
+        MusicAudioComponent.volume = currentVolume;
     }
 }
